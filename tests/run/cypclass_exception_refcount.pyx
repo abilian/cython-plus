1bb76726c69ac607dd1f23e19ca459951448a1c0
Xavier Thompson <xavier.thompson@nexedi.com>
2020-08-25 16:02:38 +0200

    Add unit test for cypclass refcounting after exceptions

---
+++ tests/run/cypclass_exception_refcount.pyx
@@ -0,0 +1,22 @@
+# mode: run
+# tag: cpp, cpp11
+# cython: experimental_cpp_class_def=True, language_level=2
+
+cdef cypclass Refcounted nolock:
+    pass
+
+cdef int raises(Refcounted r) except 0:
+    raise Exception
+
+def test_exception_refcount_cleanup():
+    """
+    >>> test_exception_refcount_cleanup()
+    2
+    """
+    r = Refcounted()
+    for i in range(50):
+        try:
+            raises(r)
+        except:
+            pass
+    return Cy_GETREF(r)

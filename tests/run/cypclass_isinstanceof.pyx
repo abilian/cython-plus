1b281f5914150268900523c5bec885a99a4ac762
Xavier Thompson <xavier.thompson@nexedi.com>
2020-09-30 13:55:33 +0200

    Add unit test for isinstanceof with const cypclass arguments

--- tests/run/cypclass_isinstanceof.pyx
+++ tests/run/cypclass_isinstanceof.pyx
@@ -26,3 +26,22 @@ def test_insinstanceof():
     cdef int r6 = isinstanceof[Derived](derived)
 
     print(r1, r2, r3, r4, r5, r6)
+
+def test_const_insinstanceof():
+    """
+    >>> test_const_insinstanceof()
+    (1, 1, 1, 0, 1, 1)
+    """
+    cdef const Base base = Base()
+    cdef const Base derived_as_base = Derived()
+    cdef const Derived derived = Derived()
+
+    cdef int r1 = isinstanceof[Base](base)
+    cdef int r2 = isinstanceof[Base](derived_as_base)
+    cdef int r3 = isinstanceof[Base](derived)
+
+    cdef int r4 = isinstanceof[Derived](base)
+    cdef int r5 = isinstanceof[Derived](derived_as_base)
+    cdef int r6 = isinstanceof[Derived](derived)
+
+    print(r1, r2, r3, r4, r5, r6)

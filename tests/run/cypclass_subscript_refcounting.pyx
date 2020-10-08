9572cd2d6f12ea8815a9d73698b0bcedd453789f
Xavier Thompson <xavier.thompson@nexedi.com>
2020-10-08 18:13:21 +0200

    Add unit test for reference count of called-on cypclass subscripts

--- tests/run/cypclass_subscript_refcounting.pyx
+++ tests/run/cypclass_subscript_refcounting.pyx
@@ -8,6 +8,9 @@ cdef cypclass Value:
     __dealloc__(self) with gil:
         print("Value destroyed")
 
+    int foo(self):
+        return 0
+
 def test_cpp_index_refcounting():
     """
     >>> test_cpp_index_refcounting()
@@ -56,5 +59,47 @@ def test_cyp_index_refcounting():
     v.vec.push_back(Value())
     a = v[0]
     if Cy_GETREF(a) != 3:
+        return -1
+    return 0
+
+def test_call_on_cpp_index_refcounting():
+    """
+    >>> test_call_on_cpp_index_refcounting()
+    Value destroyed
+    0
+    """
+    cdef vector[Value] vec
+    val = Value()
+    vec.push_back(val)
+    vec[0].foo()
+    if Cy_GETREF(val) != 3:
+        return -1
+    return 0
+
+def test_call_on_cyp_template_index_refcounting():
+    """
+    >>> test_call_on_cyp_template_index_refcounting()
+    Value destroyed
+    0
+    """
+    v = Vector[Value]()
+    val = Value()
+    v.vec.push_back(val)
+    v[0].foo()
+    if Cy_GETREF(val) != 3:
+        return -1
+    return 0
+
+def test_call_on_cyp_index_refcounting():
+    """
+    >>> test_call_on_cyp_index_refcounting()
+    Value destroyed
+    0
+    """
+    v = ValueVector()
+    val = Value()
+    v.vec.push_back(val)
+    v[0].foo()
+    if Cy_GETREF(val) != 3:
         return -1
     return 0
